09e5f21611b955c091285546916750e50d0a1094
Alfred <AlfredArvidsson@Protonmail.com>
2020-04-21 09:33:12 +0200

    Började försöka med php sql-connection

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head></head>
+
+<body>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+</body>
+
+</html>
